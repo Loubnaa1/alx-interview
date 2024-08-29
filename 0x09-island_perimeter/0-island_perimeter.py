b7302d04976e43f2e5e94484a53fae558351ba5c
Loubnaa1 <loubnamerzak4@gmail.com>
2024-08-29 23:40:07 +0000

    island perrimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """
+    Function that calculate the perimeter of the island in the grid.
+    """
+    flag = 0
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+
+                if i == 0 or grid[i - 1][j] == 0:
+                    flag += 1
+
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    flag += 1
+
+                if j == 0 or grid[i][j - 1] == 0:
+                    flag += 1
+
+                if j == cols - 1 or grid[i][j + 1] == 0:
+                    flag += 1
+
+    return flag
