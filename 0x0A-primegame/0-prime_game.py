f613d07ae246d45e290af824c33629de61b79068
Loubnaa1 <loubnamerzak4@gmail.com>
2024-09-06 00:54:08 +0000

    my proj

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-''' prime game '''
-
 
 def isWinner(x, nums):
     if x < 1 or not nums:
         return None
 
-    maria, ben = 0, 0
+    max_n = max(nums)
+    primes = [True] * (max_n + 1)
+    p = 2
+    while p * p <= max_n:
+        if primes[p]:
+            for i in range(p * p, max_n + 1, p):
+                primes[i] = False
+        p += 1
+    prime_counts = [0] * (max_n + 1)
+    for i in range(2, max_n + 1):
+        prime_counts[i] = prime_counts[i - 1] + (1 if primes[i] else 0)
+
+    maria_wins = 0
+    ben_wins = 0
     for n in nums:
-        numbers = list(range(1, n + 1))
-        current_player = "Maria"
-        primes_found = []
+        if prime_counts[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
 
-        i = 2
-        while i <= n:
-            is_prime = True
-            for p in primes_found:
-                if i % p == 0:
-                    is_prime = False
-                    break
-
-            if is_prime:
-                primes_found.append(i)
-                numbers = [num for num in numbers if num == 1 or num % i != 0]
-                current_player = "Ben" if current_player == "Maria" else "Maria"
-            i += 1
-
-        maria += current_player == "Ben"
-        ben += current_player == "Maria"
-
-    return "Maria" if maria > ben else "Ben" if ben > maria else None
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
